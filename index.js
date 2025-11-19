3be06727a59bd33b10d7eb7166ff6c8f7edd822c
frank <110762285+blood-0@users.noreply.github.com>
2025-11-19 19:01:18 -0300

    Exercises 3.1-3.6:Phonebook backend API

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 const persons = [
     { 
       "id": 1,
@@ -26,6 +28,59 @@ const persons = [
 
 app.get('/api/persons',(request,response)=>{
     response.json(persons)
+})
+
+app.get('/api/persons/:id',(request, response)=>{
+  const id = Number (request.params.id)
+  const person = persons.find(person => person.id === id)
+  if(person){
+    response.json(person)
+  }else{
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id',(request, response)=>{
+  const id = Number(request.params.id)
+  const person = persons.filter(person=>person.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/persons',(request, response)=>{
+  const body = request.body
+  
+  if(!body.name || !body.number){
+    return response.status(400).json({
+      error:'number or name missing'
+    })
+  }
+  const nameExist = persons.find(person=>person.name === body.name)
+  if(nameExist){
+    return response.status(400).json({
+      error:'name must be unique'
+    })
+  }
+
+  const generateId = () =>{
+    const maxId= 1000000
+    return Math.floor(Math.random()* maxId)
+  }
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+  persons = persons.concat(person)
+  response.json(person)
+
+})
+
+app.get('/info',(request, response)=>{
+  const requestTime= new Date()
+  response.send(`
+     <p>Phonebook has info for ${persons.length} people</p>
+     <p>${requestTime}</p>
+  `)
 })
 
 const PORT = 3001
